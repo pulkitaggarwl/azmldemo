0c8519ad97854ca430172d25bea145bd06ad7877
Pulkit Agarwal <Pulkit.Agarwal@microsoft.com>
2018-07-11 06:43:16 +0000

    Updated train.py

--- train.py
+++ train.py
@@ -10,9 +10,9 @@ import pickle
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],
      [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
 
-Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female', 'female', 'male', 'male']
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female', 'female', 'male', 'female']
 
-#Y = ['female', 'female', 'male', 'male', 'female', 'female', 'male', 'male', 'male', 'female', 'female']
+
 
 clf = SVC()
 clf = clf.fit(X, Y)
